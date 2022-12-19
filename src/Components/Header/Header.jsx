56b7d5bea39ebe40a7a2c921b16d52c06776d133
guyandjack <guillaume-d@tutanota.com>
2022-12-19 16:40:57 +0100

    component sous-menu

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,41 +3,40 @@
 //Import du module "Link"
 import { Link } from "react-router-dom";
 
+//Import des "hook"
+import { useState } from "react";
+
+//Import des composants enfants
+import { SousMenu } from "../Sous_menu/Sous__menu.jsx";
+
 //Import des feuilles de style
 import "../../Style/CSS/header.css";
 
 
 //Fonction "Header"
 function Header() {
+
+    const [display, setDisplay] = useState(false);
+
     return (
-        <div>
-            <header className="header">
-                <nav>
-                    <ul className="header-menu">
-                        <li><Link to="/"><img src="" alt=""></img></Link></li>
-                        <li><Link to="/">Accueil</Link></li>
-                        <li><Link to="/">Rappel des fondamantaux</Link></li>
-                        <li>Schemas-électriques
-                            
-                            <ul className="header-sous-menu">
-                                
-                                <li>Prise de courant</li>
-                                <li>Circuits specialisés</li>
-                                <li>Circuits éclairage</li>
-                                <li>Tableau de répartition</li>
-                                <li>Tableau de communication</li>
-                                <li>Dispositifs Differentiel Residuel (DDR)</li>
-                                <li>Disjoncteur de branchement</li>
-                                <li>Disjoncteur bipolaire</li>
+        
+        <header>
+            <nav className="header">
+                <Link to="/"><img src="" alt=""></img></Link>
+                <ul className="header__menu">
+                    <li><Link to="/">Accueil</Link></li>
+                    <li><Link to="/">Rappel des fondamentaux</Link></li>
+                    <li className="schema-elec">Schemas-électriques
+                        
+                        {display ? (<SousMenu />) : (null)}
 
-                            </ul>
-                        </li>
-                        <li>Contact</li>
+                    </li>
+                    <li>Contact</li>
 
-                    </ul>
-                </nav>
-            </header>
-        </div>
+                </ul>
+            </nav>
+        </header>
+        
     )
 }
 
---
+++ src/Components/Sous_menu/Sous__menu.jsx
@@ -0,0 +1,26 @@
+//Composant "SousMenu"
+
+//Import des feuilles de style
+
+//Fonction "SousMenu"
+
+function SousMenu() {
+    
+    return (
+        
+        <ul className="header-sous-menu">
+                            
+            <li>Prise de courant</li>
+            <li>Circuits specialisés</li>
+            <li>Circuits éclairage</li>
+            <li>Tableau de répartition</li>
+            <li>Tableau de communication</li>
+            <li>Dispositifs Differentiel Residuel (DDR)</li>
+            <li>Disjoncteur de branchement</li>
+            <li>Disjoncteur bipolaire</li>
+
+        </ul> 
+    )
+}
+
+export {SousMenu}

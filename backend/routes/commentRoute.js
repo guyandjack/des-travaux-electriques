b37d8d8d1fab43c46a6de9ccbd5d3b2eb6e2c287
guyandjack <guillaume-d@tutanota.com>
2023-08-17 22:31:02 +0200

    updates routes

--- backend/routes/commentRoute.js
+++ backend/routes/commentRoute.js
@@ -18,44 +18,68 @@ const router = express.Router();
 
 //route "get"
 router.get("/", (req, res) => {
-  res.status(200).json({ "message ": "tous les commentaires récupérés" });
+  res.status(200).json({ "message ": "tous les commentaires récupérés" })
+  .catch((err)=>{
+      res.status(500).json({err})
+  })
 });
 
 router.get("/:id", (req, res) => {
-  res.status(200).json({ "message ": "un seul  commentaire récupéré" });
+  res.status(200).json({ "message ": "un seul  commentaire récupéré" })
+  .catch((err)=>{
+      res.status(500).json({err})
+  })
 });
 
 router.get("/guigui", (req, res) => {
   res
     .status(200)
-    .json({ "message ": "tous les commentaires de guigui récupérés" });
+    .json({ "message ": "tous les commentaires de guigui récupérés" })
+    .catch((err)=>{
+        res.status(500).json({err})
+    })
 });
 
 //route "post"
 router.post("/", (req, res) => {
   res
     .status(201)
-    .json({ message: "commentaire user enregistrés dans la bdd " });
+    .json({ "message": "commentaire user enregistrés dans la bdd " })
+    .catch((err)=>{
+        res.status(500).json({err})
+    })
 });
 
 //route "delete"
 router.delete("/", (req, res) => {
   res
     .status(201)
-    .json({ message: "Tous les commentaire user suprimés de la bdd " });
+    .json({ "message": "Tous les commentaire user suprimés de la bdd " })
+    .catch((err)=>{
+        res.status(500).json({err})
+    })
 });
 
 router.delete("/:", (req, res) => {
-  res.status(201).json({ message: "commentaire user suprimé de la bdd " });
+  res.status(201).json({ message: "un commentaire user suprimé de la bdd " })
+  .catch((err)=>{
+      res.status(500).json({err})
+  })
 });
 
 //route "put"
 router.put("/", (req, res) => {
-  res.status(201).json({ message: "commentaire user mis à jour " });
+  res.status(201).json({ "message": "tous les commentaires user mis à jour " })
+  .catch((err)=>{
+      res.status(500).json({err})
+  })
 });
 
 router.put("/:id", (req, res) => {
-  res.status(201).json({ message: "commentaire user mis à jour " });
+  res.status(201).json({ "message": "Un seul commentaire user mis à jour " })
+  .catch((err)=>{
+      res.status(500).json({err})
+  })
 });
 
 module.exports = router;
